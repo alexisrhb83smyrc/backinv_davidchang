22735e96250348c8621d62daa1bf7ce07b7b319a
David <ley.chang@hotmail.com>
2024-03-11 15:50:25 -0600

    Handle invalid URL

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use("/api", routes);
 swagger(app);
 
+app.all("*", (req, res) => {
+  res.status(404).send("Invalid Url, go to api-docs/ for more info :)");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
